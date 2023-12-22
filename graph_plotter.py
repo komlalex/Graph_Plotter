545f82ee6819e5658135df1e2db46ea00636f4a1
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 13:14:56 +0000

    Bar chart

--- graph_plotter.py
+++ graph_plotter.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt 
 
-x = [2,4,5, 10]
-y = [2,3,6,10]
+x1 = [2,4,5, 10]
+y1 = [2,3,6,10]
 
+x2 = [2,4,6,8,10]
+y2 = [6,7,8,9,10]
 
-plt.plot(x, y)
+plt.plot(x1, y1, color = "green", linewidth =3,  linestyle = "dashed", marker = "o", markerfacecolor="blue", markersize =12,  label = "Line 1")
+plt.plot(x2, y2, label = "Line 2")
 
+#plt.xlim()
+# plt.ylim()
 plt.xlabel("X Axis")
 plt.ylabel("Y Axis")
 
-plt.title("Demo Graph")
+plt.title("Demo Graph - Two Line")
+
+plt.legend()
 
 plt.show()
 
